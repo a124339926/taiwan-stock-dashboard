f7f007c80c174e433662106cf5c66917d7898445
a124339926 <a124339926@users.noreply.github.com>
2026-06-18 16:26:12 +0800

    auto-update dashboard v4 (K線+產業drill-down+市場狀態)

--- screener_data.js
+++ screener_data.js
@@ -1,5 +1,5 @@
 var SCREENER = {
-  "update_time": "2026-06-18 13:35:00",
+  "update_time": "2026-06-18 16:23:45",
   "update_date": "2026年06月18日",
   "total_scanned": 2681,
   "total_matched": 30,
@@ -28,9 +28,23 @@ var SCREENER = {
       "ma5": 10.0,
       "ma10": 9.9,
       "ma20": 9.3,
-      "volume": 4339183,
+      "volume": 4648698,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量2.0x, 10日+12%, 站穩MA20"
+    },
+    {
+      "code": "1475",
+      "name": "業旺",
+      "industry": "紡織纖維",
+      "close": 27.4,
+      "change_amount": 0.1,
+      "change_pct": 0.37,
+      "ma5": 27.2,
+      "ma10": 26.9,
+      "ma20": 26.0,
+      "volume": 52027,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.5x, 10日+6%, 站穩MA20"
     },
     {
       "code": "1725",
@@ -42,79 +56,121 @@ var SCREENER = {
       "ma5": 34.3,
       "ma10": 33.1,
       "ma20": 32.5,
-      "volume": 758045,
+      "volume": 767045,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量2.5x, 10日+12%, 站穩MA20"
+    },
+    {
+      "code": "2347",
+      "name": "聯強",
+      "industry": "資訊服務",
+      "close": 93.4,
+      "change_amount": 1.0,
+      "change_pct": 1.08,
+      "ma5": 91.7,
+      "ma10": 90.6,
+      "ma20": 88.8,
+      "volume": 11081875,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.7x, 10日+6%, 站穩MA20"
     },
     {
       "code": "2520",
       "name": "冠德",
       "industry": "建材營造",
-      "close": 36.5,
-      "change_amount": 0.3,
-      "change_pct": 0.83,
+      "close": 36.4,
+      "change_amount": 0.2,
+      "change_pct": 0.69,
       "ma5": 35.4,
       "ma10": 34.2,
       "ma20": 32.4,
-      "volume": 9776328,
+      "volume": 10634032,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.8x, 10日+14%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.9x, 10日+13%, 站穩MA20"
     },
     {
       "code": "2536",
       "name": "宏普",
       "industry": "建材營造",
-      "close": 22.2,
-      "change_amount": 0.9,
-      "change_pct": 3.97,
-      "ma5": 21.7,
+      "close": 22.4,
+      "change_amount": 1.0,
+      "change_pct": 4.44,
+      "ma5": 21.8,
       "ma10": 21.4,
-      "ma20": 20.5,
-      "volume": 3823843,
+      "ma20": 20.6,
+      "volume": 4126603,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.6x, 10日+8%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.7x, 10日+8%, 站穩MA20"
     },
     {
-      "code": "3002",
-      "name": "歐格",
-      "industry": "電腦及週邊",
-      "close": 22.5,
-      "change_amount": 1.6,
-      "change_pct": 7.42,
-      "ma5": 20.2,
-      "ma10": 19.8,
-      "ma20": 19.1,
-      "volume": 4217466,
+      "code": "2838",
+      "name": "聯邦銀",
+      "industry": "金融保險",
+      "close": 22.8,
+      "change_amount": 0.3,
+      "change_pct": 1.34,
+      "ma5": 22.2,
+      "ma10": 21.8,
+      "ma20": 21.2,
+      "volume": 7566050,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量3.0x, 10日+13%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.1x, 10日+6%, 站穩MA20"
     },
     {
-      "code": "3034",
-      "name": "聯詠",
-      "industry": "半導體",
-      "close": 517.0,
-      "change_amount": 2.0,
-      "change_pct": 0.39,
-      "ma5": 503.1,
-      "ma10": 492.8,
-      "ma20": 491.5,
-      "volume": 8159869,
+      "code": "2880",
+      "name": "華南金",
+      "industry": "金融保險",
+      "close": 38.3,
+      "change_amount": 0.2,
+      "change_pct": 0.66,
+      "ma5": 37.3,
+      "ma10": 36.1,
+      "ma20": 34.3,
+      "volume": 67862220,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.6x, 10日+5%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.8x, 10日+10%, 站穩MA20"
+    },
+    {
+      "code": "2884",
+      "name": "玉山金",
+      "industry": "金融保險",
+      "close": 35.4,
+      "change_amount": -0.1,
+      "change_pct": -0.28,
+      "ma5": 35.0,
+      "ma10": 34.3,
+      "ma20": 33.0,
+      "volume": 97594278,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.6x, 10日+6%, 站穩MA20"
+    },
+    {
+      "code": "2886",
+      "name": "兆豐金",
+      "industry": "金融保險",
+      "close": 45.5,
+      "change_amount": 0.4,
+      "change_pct": 0.89,
+      "ma5": 44.5,
+      "ma10": 43.6,
+      "ma20": 42.0,
+      "volume": 72429132,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.9x, 10日+8%, 站穩MA20"
     },
     {
       "code": "3652",
       "name": "精聯",
       "industry": "電腦及週邊",
-      "close": 39.0,
-      "change_amount": 1.8,
-      "change_pct": 4.83,
-      "ma5": 37.0,
-      "ma10": 34.9,
-      "ma20": 34.4,
-      "volume": 2684883,
+      "close": 40.2,
+      "change_amount": 3.0,
+      "change_pct": 7.92,
+      "ma5": 37.2,
+      "ma10": 35.0,
+      "ma20": 34.5,
+      "volume": 2845983,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.3x, 10日+15%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.4x, 10日+18%, 站穩MA20"
     },
     {
       "code": "6142",
@@ -126,7 +182,7 @@ var SCREENER = {
       "ma5": 8.9,
       "ma10": 8.7,
       "ma20": 8.5,
-      "volume": 4509586,
+      "volume": 4564908,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量2.9x, 10日+10%, 站穩MA20"
     },
@@ -134,43 +190,57 @@ var SCREENER = {
       "code": "6166",
       "name": "凌華",
       "industry": "電腦及週邊",
-      "close": 154.5,
-      "change_amount": 12.0,
-      "change_pct": 8.42,
-      "ma5": 140.9,
-      "ma10": 135.3,
+      "close": 155.5,
+      "change_amount": 13.0,
+      "change_pct": 9.12,
+      "ma5": 141.1,
+      "ma10": 135.4,
       "ma20": 133.0,
-      "volume": 5322103,
+      "volume": 5706616,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.6x, 10日+16%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.7x, 10日+17%, 站穩MA20"
     },
     {
       "code": "6177",
       "name": "達麗",
       "industry": "建材營造",
-      "close": 49.2,
-      "change_amount": 0.9,
-      "change_pct": 1.87,
-      "ma5": 47.7,
-      "ma10": 46.8,
+      "close": 50.4,
+      "change_amount": 2.2,
+      "change_pct": 4.46,
+      "ma5": 48.0,
+      "ma10": 46.9,
       "ma20": 45.0,
-      "volume": 7032440,
+      "volume": 8027736,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.3x, 10日+8%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.5x, 10日+11%, 站穩MA20"
+    },
+    {
+      "code": "8464",
+      "name": "億豐",
+      "industry": "存託憑證",
+      "close": 358.0,
+      "change_amount": 8.5,
+      "change_pct": 2.43,
+      "ma5": 350.2,
+      "ma10": 341.4,
+      "ma20": 329.9,
+      "volume": 1287911,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.6x, 10日+9%, 站穩MA20"
     },
     {
       "code": "00666R",
       "name": "富邦恒生國企反1",
       "industry": "ETF",
-      "close": 9.6,
+      "close": 9.7,
       "change_amount": 0.2,
-      "change_pct": 1.58,
+      "change_pct": 1.9,
       "ma5": 9.4,
       "ma10": 9.3,
       "ma20": 9.2,
-      "volume": 100038,
+      "volume": 101038,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.9x, 10日+5%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.9x, 10日+6%, 站穩MA20"
     },
     {
       "code": "3537",
@@ -182,7 +252,7 @@ var SCREENER = {
       "ma5": 80.4,
       "ma10": 80.2,
       "ma20": 77.4,
-      "volume": 12209149,
+      "volume": 12337523,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量1.9x, 10日+7%, 站穩MA20"
     },
@@ -190,13 +260,13 @@ var SCREENER = {
       "code": "4716",
       "name": "大立",
       "industry": "化學",
-      "close": 18.3,
-      "change_amount": 1.1,
-      "change_pct": 6.4,
+      "close": 18.2,
+      "change_amount": 1.0,
+      "change_pct": 6.1,
       "ma5": 17.4,
       "ma10": 17.0,
       "ma20": 16.2,
-      "volume": 2617150,
+      "volume": 2695552,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量1.7x, 10日+9%, 站穩MA20"
     },
@@ -204,15 +274,15 @@ var SCREENER = {
       "code": "5432",
       "name": "新門",
       "industry": "數位雲端",
-      "close": 152.5,
-      "change_amount": 11.5,
-      "change_pct": 8.16,
-      "ma5": 143.0,
-      "ma10": 137.9,
-      "ma20": 137.2,
-      "volume": 448387,
+      "close": 154.0,
+      "change_amount": 13.0,
+      "change_pct": 9.22,
+      "ma5": 143.3,
+      "ma10": 138.1,
+      "ma20": 137.3,
+      "volume": 490448,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.7x, 10日+13%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.8x, 10日+14%, 站穩MA20"
     },
     {
       "code": "5489",
@@ -224,79 +294,93 @@ var SCREENER = {
       "ma5": 41.9,
       "ma10": 40.8,
       "ma20": 40.1,
-      "volume": 528835,
+      "volume": 529085,
       "score": 100,
       "flags": "創新高, 多頭排列, 放量2.6x, 10日+15%, 站穩MA20"
+    },
+    {
+      "code": "5508",
+      "name": "永信建",
+      "industry": "建材營造",
+      "close": 61.7,
+      "change_amount": 0.6,
+      "change_pct": 0.98,
+      "ma5": 58.4,
+      "ma10": 56.5,
+      "ma20": 52.5,
+      "volume": 3738713,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.6x, 10日+10%, 站穩MA20"
     },
     {
       "code": "6175",
       "name": "立敦",
       "industry": "電子零組件",
-      "close": 112.5,
-      "change_amount": 5.0,
-      "change_pct": 4.65,
-      "ma5": 104.9,
-      "ma10": 102.9,
+      "close": 114.0,
+      "change_amount": 6.5,
+      "change_pct": 6.05,
+      "ma5": 105.2,
+      "ma10": 103.0,
       "ma20": 99.3,
-      "volume": 26810942,
+      "volume": 28537998,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.6x, 10日+16%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.7x, 10日+18%, 站穩MA20"
+    },
+    {
+      "code": "6179",
+      "name": "亞通",
+      "industry": "其他",
+      "close": 26.8,
+      "change_amount": 1.1,
+      "change_pct": 4.48,
+      "ma5": 25.1,
+      "ma10": 24.3,
+      "ma20": 24.3,
+      "volume": 3757532,
+      "score": 100,
+      "flags": "創新高, 多頭排列, 放量1.5x, 10日+13%, 站穩MA20"
     },
     {
       "code": "6859",
       "name": "伯特光",
       "industry": "光電",
-      "close": 140.0,
-      "change_amount": 1.0,
-      "change_pct": 0.72,
-      "ma5": 131.2,
-      "ma10": 129.3,
-      "ma20": 127.6,
-      "volume": 846602,
+      "close": 139.0,
+      "change_amount": 0.0,
+      "change_pct": 0.0,
+      "ma5": 131.0,
+      "ma10": 129.2,
+      "ma20": 127.5,
+      "volume": 913025,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.0x, 10日+6%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.1x, 10日+5%, 站穩MA20"
     },
     {
       "code": "7715",
       "name": "裕山",
       "industry": "數位雲端",
-      "close": 32.6,
-      "change_amount": 2.6,
-      "change_pct": 8.67,
+      "close": 32.4,
+      "change_amount": 2.4,
+      "change_pct": 8.0,
       "ma5": 30.2,
       "ma10": 29.0,
       "ma20": 28.6,
-      "volume": 884122,
+      "volume": 919558,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.0x, 10日+17%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.1x, 10日+17%, 站穩MA20"
     },
     {
       "code": "8088",
       "name": "品安",
       "industry": "半導體",
-      "close": 69.3,
-      "change_amount": 4.5,
-      "change_pct": 6.94,
+      "close": 69.2,
+      "change_amount": 4.4,
+      "change_pct": 6.79,
       "ma5": 64.6,
       "ma10": 61.9,
-      "ma20": 61.9,
-      "volume": 14994111,
+      "ma20": 61.8,
+      "volume": 18128071,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量2.4x, 10日+10%, 站穩MA20"
-    },
-    {
-      "code": "8358",
-      "name": "金居",
-      "industry": "電子零組件",
-      "close": 699.0,
-      "change_amount": 59.0,
-      "change_pct": 9.22,
-      "ma5": 633.8,
-      "ma10": 605.6,
-      "ma20": 592.6,
-      "volume": 4664341,
-      "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.9x, 10日+17%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量2.6x, 10日+10%, 站穩MA20"
     },
     {
       "code": "4565",
@@ -308,23 +392,23 @@ var SCREENER = {
       "ma5": 118.7,
       "ma10": 118.0,
       "ma20": 117.9,
-      "volume": 140851,
+      "volume": 144074,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量3.3x, 10日+6%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量3.4x, 10日+6%, 站穩MA20"
     },
     {
-      "code": "7798",
-      "name": "將捷",
-      "industry": "建材營造",
-      "close": 17.2,
-      "change_amount": 0.2,
-      "change_pct": 0.88,
-      "ma5": 17.0,
-      "ma10": 16.8,
-      "ma20": 16.5,
-      "volume": 129567,
+      "code": "7899",
+      "name": "景美",
+      "industry": "半導體",
+      "close": 533.0,
+      "change_amount": 39.5,
+      "change_pct": 8.0,
+      "ma5": 484.0,
+      "ma10": 472.1,
+      "ma20": 463.6,
+      "volume": 484536,
       "score": 100,
-      "flags": "創新高, 多頭排列, 放量1.8x, 10日+6%, 站穩MA20"
+      "flags": "創新高, 多頭排列, 放量1.7x, 10日+9%, 站穩MA20"
     },
     {
       "code": "1432",
@@ -336,91 +420,7 @@ var SCREENER = {
       "ma5": 17.4,
       "ma10": 17.3,
       "ma20": 17.0,
-      "volume": 937930,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量2.0x, 10日+2%, 站穩MA20"
-    },
-    {
-      "code": "1708",
-      "name": "東鹼",
-      "industry": "化學",
-      "close": 50.3,
-      "change_amount": 4.5,
-      "change_pct": 9.95,
-      "ma5": 47.5,
-      "ma10": 46.8,
-      "ma20": 43.1,
-      "volume": 21103429,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量1.8x, 10日+2%, 站穩MA20"
-    },
-    {
-      "code": "5283",
-      "name": "禾聯碩",
-      "industry": "電器電纜",
-      "close": 54.4,
-      "change_amount": 1.5,
-      "change_pct": 2.84,
-      "ma5": 53.0,
-      "ma10": 52.5,
-      "ma20": 52.5,
-      "volume": 260879,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量2.5x, 10日+4%, 站穩MA20"
-    },
-    {
-      "code": "00680L",
-      "name": "元大美債20正2",
-      "industry": "ETF",
-      "close": 7.1,
-      "change_amount": 0.1,
-      "change_pct": 1.28,
-      "ma5": 7.0,
-      "ma10": 6.9,
-      "ma20": 6.9,
-      "volume": 32258839,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量1.7x, 10日+3%, 站穩MA20"
-    },
-    {
-      "code": "00897",
-      "name": "富邦基因免疫生技",
-      "industry": "ETF",
-      "close": 10.1,
-      "change_amount": 0.2,
-      "change_pct": 1.92,
-      "ma5": 9.9,
-      "ma10": 9.8,
-      "ma20": 9.8,
-      "volume": 1866854,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量2.5x, 10日+3%, 站穩MA20"
-    },
-    {
-      "code": "2221",
-      "name": "大甲",
-      "industry": "其他",
-      "close": 48.3,
-      "change_amount": 2.4,
-      "change_pct": 5.34,
-      "ma5": 45.0,
-      "ma10": 44.4,
-      "ma20": 43.6,
-      "volume": 1177679,
-      "score": 93,
-      "flags": "創新高, 多頭排列, 放量1.7x, 10日+3%, 站穩MA20"
-    },
-    {
-      "code": "5516",
-      "name": "雙喜",
-      "industry": "建材營造",
-      "close": 11.1,
-      "change_amount": 0.3,
-      "change_pct": 2.79,
-      "ma5": 10.9,
-      "ma10": 10.9,
-      "ma20": 10.6,
-      "volume": 153074,
+      "volume": 1054321,
       "score": 93,
       "flags": "創新高, 多頭排列, 放量2.1x, 10日+2%, 站穩MA20"
     }
@@ -428,30 +428,30 @@ var SCREENER = {
   "industry_distribution": [
     {
       "name": "建材營造",
-      "count": 5
+      "count": 4
     },
     {
-      "name": "化學",
-      "count": 3
-    },
-    {
-      "name": "電腦及週邊",
-      "count": 3
-    },
-    {
-      "name": "ETF",
-      "count": 3
-    },
-    {
-      "name": "電子零組件",
-      "count": 3
+      "name": "金融保險",
+      "count": 4
     },
     {
       "name": "其他",
       "count": 2
     },
     {
-      "name": "半導體",
+      "name": "紡織纖維",
+      "count": 2
+    },
+    {
+      "name": "化學",
+      "count": 2
+    },
+    {
+      "name": "電腦及週邊",
+      "count": 2
+    },
+    {
+      "name": "電子零組件",
       "count": 2
     },
     {
@@ -459,11 +459,23 @@ var SCREENER = {
       "count": 2
     },
     {
-      "name": "紡織纖維",
+      "name": "半導體",
+      "count": 2
+    },
+    {
+      "name": "資訊服務",
       "count": 1
     },
     {
       "name": "通信網路",
+      "count": 1
+    },
+    {
+      "name": "存託憑證",
+      "count": 1
+    },
+    {
+      "name": "ETF",
       "count": 1
     },
     {
@@ -480,10 +492,6 @@ var SCREENER = {
     },
     {
       "name": "家居生活",
-      "count": 1
-    },
-    {
-      "name": "電器電纜",
       "count": 1
     }
   ]
